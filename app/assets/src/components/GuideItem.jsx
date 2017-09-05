b2940619aef65401766af7455b4be3d379a4ffef
Richard Walker <richard.x.walker@gmail.com>
2017-09-05 14:25:47 +0100

    Status added to guide steps.

--- app/assets/src/components/GuideItem.jsx
+++ app/assets/src/components/GuideItem.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { observer } from "mobx-react";
 
 const GuideItem = ({step}) => (
-    <div className="panel">
+    <div className={"panel panel--"+step.status}>
         <h2>{step.title}</h2>
         <p>{step.text}</p>
     </div>
--- app/assets/src/models/Guide.js
+++ app/assets/src/models/Guide.js
@@ -4,11 +4,11 @@ import Step from './Step'
 export default class Conversation {
   @observable steps = [
     {key: "choose", title:"Choose your property", text:"21 Cotham Lawn Road, Bristol", status:"complete"},
-    {key: "offer", title:"Make an offer", text:"Not complete", status:""},
-    {key: "insure", title:"Insure property", text:"Not complete", status:""},
-    {key: "mortgage", title:"Instant mortgage", text:"Not complete", status:""},
-    {key: "accept", title:"Offer acceptance", text:"Not complete", status:""},
-    {key: "move", title:"Move in", text:"Not complete", status:""}
+    {key: "offer", title:"Make an offer", text:"Not complete", status:"incomplete"},
+    {key: "insure", title:"Insure property", text:"Not complete", status:"incomplete"},
+    {key: "mortgage", title:"Instant mortgage", text:"Not complete", status:"incomplete"},
+    {key: "accept", title:"Offer acceptance", text:"Not complete", status:"incomplete"},
+    {key: "move", title:"Move in", text:"Not complete", status:"incomplete"}
   ];
 
   @action
