211a2ed8b9a283031cbe1a408ac5ab86fc14ea6f
Anna Thomas <annaet@uk.ibm.com>
2017-09-05 15:41:44 +0100

    Spelling fix

--- app/assets/src/models/Conversation.js
+++ app/assets/src/models/Conversation.js
@@ -4,7 +4,7 @@ import MessageModel from './MessageModel'
 export default class Conversation {
   @observable messages = [
     {
-      message: 'Hi I\'m Paul! Seems like you\'re looking to find out some information about buying a property? I\'m, going to build a personalised guide for you, but first it would be great to know if you have much experience buying properties?',
+      message: 'Hi I\'m Paul! Seems like you\'re looking to find out some information about buying a property? I\'m going to build a personalised guide for you, but first it would be great to know if you have much experience buying properties?',
       bot: true
     }
   ];
